221dc8abb2410bc51b7f03b4fad43984371d84dd
Tuha Shahid <tuhashahid.dev@gmail.com>
2024-11-06 00:04:06 +0500

    Change Ui

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "http://localhost:3000" // Local development URL
+    // apiUrl: "http://localhost:3000" // Local development URL
   };
   
