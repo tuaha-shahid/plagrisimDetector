f509151ea798efd8b61e08db71c25f3208a146da
Taha Shahid <tuhashahid.dev@gmail.com>
2026-03-19 20:42:29 +0500

    Add Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+
+// 1. Add this import at the top
+import { inject } from '@vercel/analytics';
+
+// 2. Call the inject function to start tracking
+inject();
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
