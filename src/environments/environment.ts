168405f433cee993dc18a902a226f145ee679b78
Taha Shahid <tuhashahid.dev@gmail.com>
2026-03-29 03:16:19 +0500

    add blog or textbox rule 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    // apiUrl: "https://backres-production.up.railway.app" // Production backend URL
+    apiUrl: "https://backres-production.up.railway.app" // Production backend URL
   };
